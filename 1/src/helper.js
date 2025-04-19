3cd0a4861953cccaae448b4fe229086135dc54e6
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-04-20 02:42:07 +0530

    Added brightness slider

--- 1/src/helper.js
+++ 1/src/helper.js
@@ -18,3 +18,12 @@ export const isNullOrUndefined = (data) => {
 export const changeBackgroundColor = (color = "") => {
     document.body.style.backgroundColor = color;
 }
+
+export const changeBackgroundFilter = (filter = "") => {
+    const numericValue = parseFloat(filter.match(/brightness\(([^)]+)\)/)?.[1]);
+    if (numericValue < 0) {
+        console.warn("Filter value cannot be negative. Skipping update.");
+        return;
+    }
+    document.documentElement.style.filter = filter;
+}
--- 1/src/toggleBulb.js
+++ 1/src/toggleBulb.js
@@ -1,26 +1,43 @@
-import {changeBackgroundColor, getElement, isNullOrUndefined} from './helper.js';
+import {changeBackgroundColor, changeBackgroundFilter, getElement, isNullOrUndefined} from './helper.js';
 
 export const toggle = () => {
     const imageElement = getElement(`#bulbImage`);
     const inputElement = getElement(`#toggleBulb`);
+    const brightnessSlider = getElement(`#brightnessRange`);
+    const brightnessSliderWrapper = brightnessSlider.parentElement;
 
-    if(isNullOrUndefined(imageElement) || isNullOrUndefined(inputElement)){
+    if(isNullOrUndefined(imageElement) || isNullOrUndefined(inputElement) || isNullOrUndefined(brightnessSliderWrapper)){
         alert("Please reload the page once, HTML elements are missing...");
         window.location.reload();
     }
 
-    inputElement.addEventListener("change",(e) => {
+    brightnessSliderWrapper.style.display = "none";
+
+    inputElement.addEventListener("change", (e) => {
         e.preventDefault();
         const isChecked = inputElement.checked;
-        
+
         if(isChecked){
             imageElement.classList.add("zoom");
             imageElement.src = `./src/assets/lightOnBulbPng.png`;
             changeBackgroundColor(`#fff`);
-        }else{
+            brightnessSliderWrapper.style.display = "block";
+        } else {
             imageElement.classList.remove("zoom");
             imageElement.src = `./src/assets/whiteBulb.png`;
             changeBackgroundColor(`#1c1c2e`);
+            brightnessSliderWrapper.style.display = "none";
+
+            imageElement.style.filter = `brightness(1)`;
+            changeBackgroundFilter(`brightness(1)`);
+
+            brightnessSlider.value = "1";
         }
-    })
-}
+    });
+
+    brightnessSlider.addEventListener("input", (e) => {
+        const brightnessValue = e.target.value;
+        imageElement.style.filter = `brightness(${brightnessValue})`;
+        changeBackgroundFilter(`brightness(${brightnessValue})`);
+    });
+};
